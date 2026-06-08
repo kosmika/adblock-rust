698c95249743155421a22e1ba47d9da455baee36
boocmp <boocmp@yandex.ru>
2026-06-08 17:34:50 +0700

    Compilation.

--- tests/ublock-coverage.rs
+++ tests/ublock-coverage.rs
@@ -280,7 +280,7 @@ fn check_matching_hostnames() {
             source_host.hostname(),
             &req.r#type,
             third_party,
-            None,
+            "",
         );
 
         let checked = engine.check_network_request(&request);
